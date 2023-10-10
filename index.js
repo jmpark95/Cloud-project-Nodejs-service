8553fecd2780ddbf98a1eb04d012cb97c24fe759
jmpark95 <jmpark95@live.com>
2023-10-10 13:23:28 +1100

    cloud build testing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/viewcounttt", async (req, res) => {
+app.get("/viewcount", async (req, res) => {
    const query = await db.collection("viewCountCollection").doc("viewCountDocumentID").get();
    const data = query.data();
 
